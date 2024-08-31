de3d21996c17bbf85d0fa1db5160ee38a04c4c16
Dakota <dakotawkeast@gmail.com>
2024-08-31 02:43:48 -0400

    added tailwindcss and started to rework navbar with react-icons

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,27 +1,47 @@
-import {
-    Nav,
-    NavLink,
-    Bars,
-    NavMenu
-} from './NavbarElements'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { HiCodeBracketSquare } from 'react-icons/hi2'
 
 export default function Navbar(){
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
     return(
         <>
-            <Nav>
-                <Bars/>
-                <NavMenu>
-                    <NavLink to='/about'>
-                        About
+            <nav className="navbar">
+                <div className="nav-container">
+                    <NavLink exact to="/" className="nav-logo">
+                        <span>D.K.</span>
+                        <span>
+                            <HiCodeBracketSquare />
+                        </span>
                     </NavLink>
-                    <NavLink to='/projects' activeStyle>
-                        Projects
-                    </NavLink>
-                    <NavLink to='/contact'>
-                        Contact
-                    </NavLink>
-                </NavMenu>
-            </Nav>
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className="nav-item">
+                            <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick}>
+                            Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick}>
+                            About
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick}>
+                            My Projects
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick}>
+                            Contact
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <GiHamburgerMenu />
+            </nav>
         </>
     )
 }
