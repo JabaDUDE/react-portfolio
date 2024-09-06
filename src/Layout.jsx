a825fe6d02a01df5a371b97d4b9a167975f553ca
Dakota <dakotawkeast@gmail.com>
2024-09-06 03:48:47 -0400

    fix viewport bug that created whitespace towards bottom on viewport

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-    <div className='bg-black h-screen text-white'>
+    <div className='bg-black min-h-screen text-white'>
         <Navbar />
         <Outlet />
     </div>
--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -2,7 +2,7 @@ import img from '../../assets/self/flower-king.jpg'
 
 function About() {
   return (
-    <div className='flex'>
+    <div className='flex max-w-screen-lg'>
       <div className='md:m-10 m-5'>
         <div className='md:float-left md:mr-5'>
           <img src={img} alt='photo of myself' className='w=[150px] h-[275px] rounded-xl m-auto' />
@@ -10,7 +10,7 @@ function About() {
       <div className='p-4'>
         <p className='mb-4'>My name is Dakota and I'm a web developer! I thrive on the excitement of learning and the challenge of problem solving. WIth a passion for discovering new concepts and mastering complex skills, I am constanty seeking opportunities to epand my knowledge and apply it. My approach to challenges is grounded in curiosity and creativity.</p>
         <p>Whether tackling projects or exploring various fields, I am dedicated to growing personally and professionally through continuous learning and problem solving.</p>
-          <div className='text-right mt-5'>
+          <div className='text-right mt-10'>
           <blockquote>
           <p><q>Journey Before Destination</q></p>
           </blockquote>
