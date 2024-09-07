b29eb10f3af81893b4e2b0b0915a1cf5780539e8
Dakota <dakotawkeast@gmail.com>
2024-09-07 00:16:20 -0400

    got rid of console.log

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -7,7 +7,6 @@ function Projects() {
       <h1 className='text-center text-xl'>Technologies I Use:</h1>
       <div className='flex justify-center flex-wrap'>
         {data.languages.map((item, index) => {
-          console.log(item)
           return (
             <img src={item.image} alt={item.name} key={index} width={40} height={40} className='m-4'/>
           )
