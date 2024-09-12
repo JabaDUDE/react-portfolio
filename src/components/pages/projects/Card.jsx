d9d1f301523edb99edc7b8b42dffdc7266012b4a
Dakota <dakotawkeast@gmail.com>
2024-09-12 18:07:42 -0400

    style of card coming along

--- src/components/pages/projects/Card.jsx
+++ src/components/pages/projects/Card.jsx
@@ -1,14 +1,14 @@
 
 export default function Card({ project: {name, technologies, image, link, github} }) {
   return (
-    <div className='max-w-45 max-h-64 border-2 border-white'>
-    <img src={image} alt={name} className='object-contain float-left h-full' width={150} />
-    <div>
-        <h1>{name}</h1>
-        <h3>Made with: {technologies}</h3>
-        <div>
-            <button href={link}>Live Site</button>
-            <button href={github}>Github</button>
+    <div className='w-80 border border-white bg-white'>
+    <img src={image} alt={name} className='w-full' width={150} />
+    <div className='p-4 text-black'>
+        <h1 className='text-gray-500 font-bold tracking-widest mb-0 uppercase'>{name}</h1>
+        <h3 className='font-bold mt-0'>Made with: {technologies}</h3>
+        <div className='flex gap-4 justify-center'>
+            <button className='bg-[#00df9a] hover:bg-green-500 text-white px-4 py-2 inline-block mt-4 rounded' href={link}>Live Site</button>
+            <button className='bg-[#00df9a] hover:bg-green-500 text-white px-4 py-2 inline-block mt-4 rounded' href={github}>Github</button>
         </div>
     </div>
     </div>
