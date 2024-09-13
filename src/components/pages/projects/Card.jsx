697cb1c46feddc69f6831a37d7df6b6a6d33252b
Dakota <dakotawkeast@gmail.com>
2024-09-13 03:51:13 -0400

    fixed issue with buttons

--- src/components/pages/projects/Card.jsx
+++ src/components/pages/projects/Card.jsx
@@ -1,13 +1,13 @@
 
 export default function Card({ project: {name, technologies, image, link, github, description} }) {
   return (
-    <div className='w-80 border bg-white rounded-xl'>
-    <img src={image} alt={name} className='w-full rounded-xl' width={150} />
-    <div className='p-4 text-black'>
+    <div className='bg-white rounded-xl w-80 flex flex-col'>
+    <img src={image} alt={name} className='w-full h-48 rounded-t-xl mb-1' />
+    <div className='text-black px-2'>
         <h1 className='text-gray-500 font-bold tracking-widest mb-0 uppercase'>{name}</h1>
         <h3 className='font-bold mt-0'>Made with: {technologies}</h3>
-        <p className='mt-2'>{description}</p>
-        <div className='flex gap-4 justify-center place-self-end border-black border-2'>
+        <p>{description}</p>
+        <div className='flex gap-4 justify-center mb-1'>
             <button className='bg-[#00df9a] hover:bg-green-500 text-white px-4 py-2 inline-block mt-4 rounded' href={link}>Live Site</button>
             <button className='bg-[#00df9a] hover:bg-green-500 text-white px-4 py-2 inline-block mt-4 rounded' href={github}>Github</button>
         </div>
--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -20,7 +20,7 @@ function Projects() {
 
       <h1 className='text-center text-xl'>Projects:</h1>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-4 my-2 items-stretch'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-4 my-2 p-4 items-stretch'>
         {Project.map((project, index) => {
           return <Card project={project} key={index} />
         })}
