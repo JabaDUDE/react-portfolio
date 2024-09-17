8571c977df8b1242bc9d28042ed8189b7a7479fd
Dakota <dakotawkeast@gmail.com>
2024-09-17 19:44:09 -0400

    improved gradient background on homepage

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="bg-black text-white h-screen flex flex-col items-center justify-center">
       <div className='text-left'>
-        <h1 className='text-7xl'>I'm <span className='bg-gradient-to-r from-[#57aa8d] to-[#19ebba] text-transparent bg-clip-text'>Dakota</span></h1>
+        <h1 className='text-7xl mb-2'>I'm <span className='bg-gradient-to-r from-[#3044c5] to-[#acfa70] text-transparent bg-clip-text'>Dakota</span></h1>
         <h3 className="text-lg">Chronically curious and forever knowledge seeking. What will I learn today?</h3>
       </div>
     </div>
